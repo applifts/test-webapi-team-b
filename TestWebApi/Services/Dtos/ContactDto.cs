7cc034af3b0f9acaa7a8fb4d35ba06077a56023c
Sam Altier <samtaltier@gmail.com>
2018-01-18 10:06:34 -0600

    Updated fields of Contact objects

--- TestWebApi/Services/Dtos/ContactDto.cs
+++ TestWebApi/Services/Dtos/ContactDto.cs
@@ -4,5 +4,15 @@
     {
         public int Id { get; set; }
         public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string CompanyName { get; set; }
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string Skillset { get; set; }
+        public string PayRate { get; set; }
     }
 }
